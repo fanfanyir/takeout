4ccf154c28a1e3717012849887bd68a669fab6da
fanfanyir <fanfanyir@163.com>
2019-02-19 01:04:27 +0800

    babel

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 const path  = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fs = require('fs');
 const srcRoot = path.resolve(__dirname, 'src');
 const devPath = path.resolve(__dirname, 'dev');
@@ -23,6 +24,7 @@ function getEntry() {
 
 const entryMap = getEntry();
 module.exports = {
+  mode: 'development',
   entry: entryMap,
   output: {
     path: devPath,
@@ -30,9 +32,13 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.(js|jsx)$/, use:[{loader: 'babel-loader'}], include: srcRoot},
       { test: /\.css$/, use:['style-loader', 'css-loader'], include: srcRoot},
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'], include: srcRoot},
       { test: /\.(png|jpg|jpeg)$/, use: 'url-loader?limit=8192', include: srcRoot}
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 }
