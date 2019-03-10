c11f7941adbb8edef5543aa2d5ec7fb1b7327aec
fanfanyir <fanfanyir@163.com>
2019-03-11 00:31:48 +0800

    add list.map and 评价

--- src/page/index/Order/ListItem/ListItem.jsx
+++ src/page/index/Order/ListItem/ListItem.jsx
@@ -5,40 +5,81 @@ class ListItem extends React.Component {
   constructor(props){
     super(props);
   }
-
+  /**
+   * 渲染每个菜品的总计
+   * @param {*} data 
+   */
+  renderTotalPrice(item, data) {
+    return (
+      <div className="product-item">
+        <span>...</span>
+        <div className="p-total-count">
+          总计{data.product_count}个菜，实付
+          <span className="total-price">￥{data.total}</span>
+        </div>
+      </div>
+    )
+  }
   /**
    * 渲染具体菜品
    * @param {*} data 
    */
-  renderProduct() {
+  renderProduct(data) {
+    let list = data.product_list;
 
+    // push 一个用来计算总计的{type: more}
+    list.push({type: 'more'});
+
+    return list.map((item, index) => {
+      if(item.type === 'more') {
+        return this.renderTotalPrice(item, data)
+      }
+      return (
+        <div className="product-item" key={index}>
+          {item.product_name}
+          <div className="p-count">
+            x {item.product_count}
+          </div>
+        </div>
+      )
+    })
   }
 
   /**
    * 评价按钮
    * @param {*} data 
    */
-  renderComment() {
+  renderComment(data) {
+    let evaluation = !data.is_comment;
 
+    if(evaluation){
+      return (
+        <div className="evaluation clearfix">
+          <div className="evaiuation-btn">评价</div>
+        </div>
+      )
+    }
+    return null;
   }
 
   render() {
+    let data = this.props.itemData;
     return (
       <div className="order-item">
         <div className="order-item-inner">
-          <img className="item-img" src=""/>
+          <img className="item-img" src={data.poi_pic}/>
           <div className="item-right">
             <div className="item-top">
-              <p className="order-name"></p>
-              <div className="arrow">xx</div>
-              <div className="order-state"></div>
+              <p className="order-name one-line">{data.poi_name}</p>
+              <div className="arrow"></div>
+              <div className="order-state">{data.status_description}</div>
             </div>
             <div className="item-bottom">
-              {this.renderProduct()}
+              {this.renderProduct(data)}
             </div>
           </div>
         </div>
-        {this.renderComment()}
+        {this.renderComment(data)}
       </div>
     )
   }
